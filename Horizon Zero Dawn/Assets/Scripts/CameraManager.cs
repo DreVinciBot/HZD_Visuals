45d209e7eca51b8c22992246fb04dd9523bf302f
DreVinciBot <35714752+DreVinciBot@users.noreply.github.com>
2020-08-24 12:52:59 -0400

    added topdown camera and build folder

---
+++ Horizon Zero Dawn/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public Camera main_cam;
+    public Camera topdown_cam;
+    bool switchCheck = true;
+
+    void Start()
+    {
+        main_cam.enabled = switchCheck;
+        topdown_cam.enabled = !switchCheck;
+    }
+
+    public void switchCamera()
+    {
+        switchCheck = !switchCheck;
+        main_cam.enabled = switchCheck;
+        topdown_cam.enabled = !switchCheck;
+    }
+
+
+}

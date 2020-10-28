8a3b2328285b8a1ccd5e4005b8a873d4ac40aef1
DreVinciBot <35714752+DreVinciBot@users.noreply.github.com>
2020-10-27 22:08:38 -0400

    update static folder

--- HZD/Assets/Scripts/Sever Scripts/Web.cs
+++ HZD/Assets/Scripts/Sever Scripts/Web.cs
@@ -13,7 +13,7 @@ public class Web : MonoBehaviour
     public bool state = false;
     public static string username_input;
 
-    private string ngrok = "https://87e7663da6d3.ngrok.io";
+    private string ngrok = "https://e4e7ea4bc7e9.ngrok.io";
 
     void Start()
     {

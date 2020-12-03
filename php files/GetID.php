4b4261e8cda627e5c73d290e8f426a46d6d13019
DreVinciBot <35714752+DreVinciBot@users.noreply.github.com>
2020-12-03 13:06:49 -0500

    Update GetID.php

--- php files/GetID.php
+++ php files/GetID.php
@@ -1,5 +1,29 @@
 <?php
+// $servername = "localhost"; //use url if server is online
+// $username = "root";
+// $password = "";
+// $dbname = "arnavigationalstudy2020";
 
-echo "Hello, today is ". date("Y-m-d H:i:s");
+$servername = "database-aws.cimfp3oazumo.us-east-2.rds.amazonaws.com"; //use url if server is online
+$username = "admin";
+$password = "human-robot";
+$dbname = "usersDB";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// $sql = "SELECT id FROM users";
+$sql = "SELECT * FROM users WHERE id=(SELECT MAX(id) FROM users)";
+$result = $conn->query($sql);
+$last_id = $result->fetch_assoc();
+
+// print_r($last_id["id"]);
+echo($last_id["id"]);
+
+$conn->close();
 ?>
